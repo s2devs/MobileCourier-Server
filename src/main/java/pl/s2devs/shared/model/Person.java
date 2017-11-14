af3a188cec32f9788be4c51f595dabc2ad267be5
Rafal <rafal@szyszka.it>
2017-11-14 20:18:42 +0100

    extracted shared library for people entities

---
+++ src/main/java/pl/s2devs/shared/model/Person.java
@@ -0,0 +1,26 @@
+package pl.s2devs.shared.model;
+
+/**
+ * Created by rafal on 14.11.17.
+ */
+public class Person {
+
+    protected String email;
+    protected String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/pl/s2devs/shared/response/RegistrationResponse.java
@@ -0,0 +1,29 @@
+package pl.s2devs.shared.response;
+
+import pl.s2devs.shared.model.Person;
+
+/**
+ * Created by rafal on 14.11.17.
+ */
+public class RegistrationResponse {
+
+    public enum Code {
+        EMAIL_TAKEN, CLIENT_REGISTERED
+    }
+
+    private Person person;
+    private Code code;
+
+    public RegistrationResponse(Person person, Code code) {
+        this.person = person;
+        this.code = code;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public Code getCode() {
+        return code;
+    }
+}
